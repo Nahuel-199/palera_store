16e2fcd1c6c09415d6283f6b5e3246a411357e1f
Nahuel Cernadas <gabrielnahuel96@gmail.com>
2023-06-09 18:33:33 -0300

    changes1

--- src/pages/singleProduct/SingleProduct.jsx
+++ src/pages/singleProduct/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { publicRequest } from "../../requestMethod";
-import "./singleproduct.css";
+import "./singleProduct.css";
 
 const SingleProduct = () => {
   const { id } = useParams()
